6d0212b82706d16909f1f6e693e93be2bb7088fb
adonisamarante <adonisamarante@gmail.com>
2024-10-23 16:47:58 -0300

    style: body background, box-shadow and line height

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,16 +10,17 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: none;
   }
 
   body {
-    background-color: ${(props) => props.theme['gray-800']};
-    color: ${(props) => props.theme['gray-100']};
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
     font: 400 1rem Nunito, sans-serif;
+    line-height: 160%;
   }
 `
