6d1189d649b8863a3ea394fdb88b5bc8a1768162
adonisamarante <adonisamarante@gmail.com>
2024-10-25 16:51:52 -0300

    feat: default layout

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  background-color: ${(props) => props.theme['base-background']};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
