4e5733fa6c0cbe752a4c66e0ea52fec27c1adc90
adonisamarante <adonisamarante@gmail.com>
2024-10-31 17:32:29 -0300

    style: input width

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const StyledInput = styled.input`
-  width: 19.85rem;
   padding: calc(0.75rem - 1px) calc(1rem - 1px);
 
   background-color: ${(props) => props.theme['base-input']};
