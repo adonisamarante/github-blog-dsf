9f77e88aadedcd4c7bd1c192407f2f477c11e600
adonisamarante <adonisamarante@gmail.com>
2024-11-04 16:34:03 -0300

    refactor: card using props information

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,26 @@
 import { Container, DescriptionWrapper, TitleWrapper } from './styles'
 
-export function Card() {
+interface IIssue {
+  id: number
+  title: string
+  date: string
+  description: string
+}
+
+interface ICardProps {
+  issue: IIssue
+  onClick: () => void
+}
+
+export function Card({ issue, onClick }: ICardProps) {
   return (
-    <Container>
+    <Container onClick={onClick}>
       <TitleWrapper>
-        <p>JavaScript data types and data structures</p>
-        <p>Há 1 dia</p>
+        <p>{issue.title}</p>
+        <p>{issue.date}</p>
       </TitleWrapper>
 
-      <DescriptionWrapper>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in JavaScript and what properties
-        they have. These can be used to build other data structures. Wherever
-        possible, comparisons with other languages are drawn. Dynamic typing
-        JavaScript is a loosely typed and dynamic language. Variables in
-        JavaScript are not directly associated with any particular value type,
-        and any variable can be assigned (and re-assigned) values of all types:
-        let foo = 42; // foo is now a number foo = &apos;bar&apos;; // foo is
-        now a string foo = true; // foo is now a boolean
-      </DescriptionWrapper>
+      <DescriptionWrapper>{issue.description}</DescriptionWrapper>
     </Container>
   )
 }
